daeee36e0451e6d3a1e301cdadce38559d855a1b
e3afrn <r.fu@e3ahealth.com>
2023-01-10 18:27:28 +0800

    Reimplement useEffect

--- src/createUseEffect.ts
+++ src/createUseEffect.ts
@@ -1,13 +1,52 @@
-export function createUseEffect(onMount: (fn: () => any) => void) {
-  return function useEffect(fn: () => void, params: any[]) {
-    function onChange(...params: any[]): void {
-      fn();
-    }
+import { onMount, beforeUpdate, afterUpdate } from "svelte";
+import type { Readable } from "svelte/store";
 
-    if (!params.length) {
-      return onMount(fn);
-    }
+export function createUseEffect() {
+  return function useEffect(fn: () => void | undefined | (() => void), deps?: Readable<unknown>[]) {
+    const depsVal = deps ? ([] as unknown[]) : undefined; // Save values of readable dependency stores
 
-    return onChange(...params);
-  };
+    let isMounted = false;
+
+    let unsub: void | undefined | (() => void); // Effect's cleanup callback
+
+    let previousDepsVal: unknown[] | undefined; // Snapshot of values of readable dependency stores
+
+    onMount(() => {
+      isMounted = true;
+      const unsubscribeDeps = deps?.map((d, i) =>
+        d.subscribe(v => {
+          if (depsVal) depsVal[i] = v;
+        })
+      ) // listen changes of readable dependency stores
+      checkDepsChange();
+      unsub = fn(); // initial run of effect happens when mounted, just like React
+      return () => {
+        unsub?.();
+        unsubscribeDeps?.forEach(u => u());
+      }
+    });
+
+    beforeUpdate(() => {
+      if (!isMounted) return; // This makes sure the first effect runs when onMount, like React
+      if (checkDepsChange()) unsub?.();
+    });
+
+    afterUpdate(() => {
+      if (checkDepsChange()) unsub = fn();
+    });
+
+    function checkDepsChange(): boolean {
+      if (!depsVal) return true; // No deps mean always change
+
+      const currentDepsVal = [...depsVal];
+      if (currentDepsVal.length !== previousDepsVal?.length) true;
+      for (let i = 0; i < currentDepsVal.length; i++) {
+        const curr = currentDepsVal[i];
+        const last = previousDepsVal?.[i];
+        if (!Object.is(curr, last)) return true; // Detects change
+      }
+      previousDepsVal = currentDepsVal;
+      return false; // no change
+    }
+  }
 }
